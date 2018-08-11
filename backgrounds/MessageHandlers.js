048329c3a786ba2462978f6265134c0f668ed252
gamerslouis <gamerslouis@gmail.com>
2018-08-12 00:34:38 +0800

    Regular Develop

--- backgrounds/MessageHandlers.js
+++ backgrounds/MessageHandlers.js
@@ -2,11 +2,30 @@ function messageHandler(msg, sender, response) {
     if (msg.command != undefined) {
         switch (msg.command) {
             case 'GetAllTabList':
-                {
-                    chrome.tabs.query({}, (tabs) => {
-                        response(tabs);
-                    });
-                }
+            {
+                chrome.tabs.query({},response);
+                break;
+                
+            }
+
+            case 'RemoveTabbyIds':
+            {
+                chrome.tabs.remove(msg.tabIds);
+                break;
+            }
+
+            case 'GetTabbyId':
+            {
+                chrome.tabs.get(msg.tabId,response);
+                break;
+            }
+
+            case "ChangeCurentTabbyId":
+            {
+                chrome.tabs.update(Number(msg.tabId), {highlighted: true});　//切換分頁
+                //chrome.tabs.highlight({'tabs':Number(request.tabId)});
+                break;
+            }
         }
     }
     return true;
--- commmon/Storage.js
+++ commmon/Storage.js
@@ -1,8 +1,20 @@
-function Storage() {
+var storage;
 
-    this.clearLocalStorage = function() {
-        chrome.storage.storageArea.clear();
-    }
 
-    this.load();
+storage.clearLocalStorage = function() {
+    log.dbg("clear local storage");
+    localStorage.clear();
+}
+
+storage.set = (key,val)=>{
+    log.dbg("storage set "+ key + " as "+ val);
+    localStorage.setItem(key,val);
+}
+
+storage.get() = (key)=>{
+    return localStorage.getItem(key);
+}
+
+storage.remove = ()=>{
+    localStorage.clear();
 }
